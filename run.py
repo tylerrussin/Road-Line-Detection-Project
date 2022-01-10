01fa02be0b25dfc0b3719b4ff8972be2cce36d9b
Tyler Russin <tylerrussin2@gmail.com>
2022-01-10 04:04:53 -0800

    Ok now we actually might have it

--- run.py
+++ run.py
@@ -76,7 +76,7 @@ BUCKET_NAME = 'road-line-detection-scenes'
 # Creating Flask and Dash servers
 
 server = Flask(__name__)
-app = dash.Dash(__name__,server=server external_stylesheets=external_stylesheets, meta_tags=meta_tags)
+app = dash.Dash(__name__,server=server, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 app.config.suppress_callback_exceptions = True
 app.title = 'Road Lane Detection App' # Browser Title
 server_run = app.server
